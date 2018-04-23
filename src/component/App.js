73d6d8c97f7b932e3f2f5fac543a0b4cf3173f11
Rajdeep Debnath <Rajdeep.Debnath@ADRAPIDVALUE.COM>
2018-04-23 13:30:19 +0530

    added babel-preset-stage-2

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {};
 
   render() {
     return (
