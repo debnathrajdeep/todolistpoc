1010c5e2d3adab36aa5260223da85408fb65f541
Rajdeep Debnath <Rajdeep.Debnath@ADRAPIDVALUE.COM>
2018-04-23 13:26:07 +0530

    added babel-preset-stage-2

--- src/component/App.js
+++ src/component/App.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 import DateTimePicker from 'react-datetime-picker';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
+  state = {
+    date: new Date(),
   }
 
   onChange(data)
   { 
     this.setState({ date : data })
+    this.onChng(data);
   }
 
+  onChng = (data) => {console.log(data)};
+  
   render() {
     return (
       <div>
         <h1>OnSolve Dev Environment</h1>
         <h3>Project basic environment setup</h3>
         <DateTimePicker
-          onChange={data => this.onChange(data)}
+          onChange={this.onChng}
           value={this.state.date}
         />
       </div>
