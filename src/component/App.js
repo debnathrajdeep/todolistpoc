84bbbe63f6145dd358a60bc8cfa012b016343f82
Rajdeep Debnath <Rajdeep.Debnath@ADRAPIDVALUE.COM>
2018-04-23 11:09:27 +0530

    Date picker

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import DateTimePicker from 'react-datetime-picker';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  onChange(data)
+  { 
+    this.setState({ date : data })
+  }
+
   render() {
     return (
       <div>
         <h1>OnSolve Dev Environment</h1>
         <h3>Project basic environment setup</h3>
+        <DateTimePicker
+          onChange={data => this.onChange(data)}
+          value={this.state.date}
+        />
       </div>
     );
   }
