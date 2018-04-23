b434f283fb550a65d309469ffe325aa1b1462e6c
Rajdeep Debnath <Rajdeep.Debnath@ADRAPIDVALUE.COM>
2018-04-23 11:12:46 +0530

    moved datepicker to new branch

--- src/component/App.js
+++ src/component/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import DateTimePicker from 'react-datetime-picker';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
-  }
-
-  onChange(data)
-  { 
-    this.setState({ date : data })
+    this.state = {};
   }
 
   render() {
@@ -17,10 +11,6 @@ export default class App extends Component {
       <div>
         <h1>OnSolve Dev Environment</h1>
         <h3>Project basic environment setup</h3>
-        <DateTimePicker
-          onChange={data => this.onChange(data)}
-          value={this.state.date}
-        />
       </div>
     );
   }
