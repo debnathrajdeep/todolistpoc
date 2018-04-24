8e14ad9aec31ac349ed6bb2a4632d1d5eb4c16c5
nishachapte <nishac189@gmail.com>
2018-04-24 17:47:40 +0530

    Merge login in existing pages with react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,29 @@
-// import React, { Component } from 'react';
-
-// export default class App extends Component {
-//   state = {};
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>OnSolve Dev Environment</h1>
-//         <h3>Project basic environment setup</h3>
-//       </div>
-//     );
-//   }
-// }
-
 
 import React, { Component } from 'react';
 import TodosList from '../components/Todos/todosList'
 import CreateTodo from './Todos/create-todo'
 import _ from 'lodash'
-import { Route, NavLink, HashRouter } from "react-router-dom";
+
 import Todo from '../components/Todos/todo'
 import Home from '../components/Home/Home'
+import Root from '../Root'
+import Login from '../components/Login/login'
+import { browserHistory, Router, Route, IndexRoute, Link,Switch} from 'react-router'
 
 
 export default class App extends Component {
 
   render() {
-
     return (
-      <HashRouter>
-        <div>
-          <center>
-            <h1>TODO APP</h1>
-          </center>
-          <ul className="header">
-            <li><NavLink to="/Home">Home</NavLink></li>
-            <li><NavLink to="/todo">ToDo-list</NavLink></li>
-          </ul>
-          <div className="content">
-            <Route path="/home" component={Home} />
-            <Route path="/todo" component={Todo} />
-          </div>
-        </div>
-      </HashRouter>
-    );
+      <Router history={browserHistory}>
+      
+        <Route path="/" component={Root} >
+          <IndexRoute component={Login} />
+          <Route path="/todo/:id" component={Todo} />
+          <Route path="/home/:id" component={Home} />
+       </Route>
+      </Router>
+    )
   }
-
 }
 
